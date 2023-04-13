16942ac5f4a346da8e31152ca502abaedbce7eff
Shareef-deen <sherifdeen968@gmail.com>
2023-04-13 22:29:44 +0000

    test

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ const nam = 'James';
 
 const person = { first: nam };
 
-console.log(person, nam);
+console.log(person,nam);
+
+console.log("trying things out");
 
 const sayHelloLinting = (fName: string) => {
 	console.log(`Hello linting, ${fName}`);
