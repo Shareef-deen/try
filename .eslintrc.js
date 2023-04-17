01cc7f5551c9ba03ca2ffc86311a19930b2eeabf
Shareef-deen <sherifdeen968@gmail.com>
2023-04-17 14:24:31 +0000

    jkhjj

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,42 @@
 module.exports = {
-	env: {
-		node: true,
-		es2017: true,
-	},
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	overrides: [],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['@typescript-eslint'],
-	rules: {
-		semi: ['error', 'always'],
-		'max-len': ['error', { code: 100 }],
-		'no-unused-vars': 'off',
-		'@typescript-eslint/no-unused-vars': ['error'],
-		'unused-imports/no-unused-imports': ['off', 'error'],
-		'no-underscore-dangle': ['error'],
-		'object-curly-spacing': ['error', 'always'],
-	},
-	root: true,
-};
+    "env": {
+        "es2017": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "indent": [ "error", 2 ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+		"no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": ["error"],
+        "max-len": ["error", { code: 100 }],
+        "object-curly-spacing": ["error", "always"],
+
+    },
+    root: true,
+}
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 //import path from "path";
+
 const nam = 'James';
 
 const person = { first: nam };
-
+// eslint-disable-next-line max-len
+const newn =
+	'djkfsjdfkjhsdkjfhkjahdjfsklajhlkjsdfhalkjfhlkajshdflkjahsdlkjfhlaksdjfklasdjlkjkjlkjlkjl';
+console.log(newn);
 console.log(person, nam);
 
 console.log('trying things ');
